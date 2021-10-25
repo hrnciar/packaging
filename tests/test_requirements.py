f1ccd3e5374e343e5db60010dddefadf648059e4
Tomáš Hrnčiar <thrnciar@redhat.com>
2021-10-25 11:25:30 +0200

    Remove LegacyVersion tests
    Handwritten parser for packaging does not support LegacyVersion. Since
    there is a plan to remove LegacyVersion support from packaging. I am
    removing it now to avoid test failures.

--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -59,16 +59,6 @@ class TestRequirements:
         req = Requirement("name>=3")
         self._assert_requirement(req, "name", specifier=">=3")
 
-    def test_with_legacy_version(self):
-        req = Requirement("name==1.0.org1")
-        self._assert_requirement(req, "name", specifier="==1.0.org1")
-
-    def test_with_legacy_version_and_marker(self):
-        req = Requirement("name>=1.x.y;python_version=='2.6'")
-        self._assert_requirement(
-            req, "name", specifier=">=1.x.y", marker='python_version == "2.6"'
-        )
-
     def test_version_with_parens_and_whitespace(self):
         req = Requirement("name (==4)")
         self._assert_requirement(req, "name", specifier="==4")
